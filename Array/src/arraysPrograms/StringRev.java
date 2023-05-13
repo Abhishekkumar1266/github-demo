300bef9c17a1cb3f6071e1a11fcdc0e5c5ec6243
Abhishek kumar <samrat840@gmail.com>
2023-05-14 00:55:49 +0530

    committing the code

--- Array/src/arraysPrograms/StringRev.java
+++ Array/src/arraysPrograms/StringRev.java
@@ -3,7 +3,7 @@ package arraysPrograms;
 public class StringRev {
 
 	public static void main(String[] args) {
-		String a="Abhishek";
+		String a="Menaka";
 		String b="";
 		
 		for(int i=a.length()-1;i>=0;i--){
